6e1f48dd5d9fa8ef2b4ef93bdb58a65ce3568c08
Muslih <muslihzarth@gmail.com>
2017-03-28 02:14:39 +0800

    lesson 2.2

---
+++ app/serializers/contact_serializer.rb
@@ -0,0 +1,4 @@
+class ContactSerializer < ActiveModel::Serializer
+  attributes :id, :family_name, :given_names, :title, :phone, :email, :website, :address, :costumer_id, :additional_info
+  has_one :company
+end
